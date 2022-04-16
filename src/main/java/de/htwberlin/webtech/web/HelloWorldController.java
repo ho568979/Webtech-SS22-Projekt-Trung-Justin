f995843ef77e1e6ae86b0d81b052ea64a2ed2851
Thanh <thanh0311@hotmail.de>
2022-04-16 19:27:45 +0200

    hello world greeting

--- src/main/java/de/htwberlin/webtech/web/HelloWorldController.java
+++ src/main/java/de/htwberlin/webtech/web/HelloWorldController.java
@@ -1,7 +1,9 @@
 package de.htwberlin.webtech.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -11,5 +13,12 @@ public class HelloWorldController {
     public ModelAndView showHelloWorldPage() {
         return new ModelAndView("helloworld");
     }
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
 
 }
